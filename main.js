d7ea8911d55d8a388b9df41743dc1a82ec775167
Daniel Vo <daniel.vo@berkeley.edu>
2022-02-01 20:14:01 -0800

    able to display api data on html

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var app = new Vue({
     el: "#app",
-    data: function () {
-        return {
-            artist : 0
-        };
-    },
+    data: {
+        artist: '',
+        song: '',
+    }
 })
-    ;
+
+const vm = app.$mount('#app')
+console.log(vm.artist)
+
+vm.artist = "Wow Bowie"
+
+fetch("https://s.rockbot.com/temp/now_playing.json")
+    .then(response => response.json())
+    .then(data => {
+        vm.artist = data.aNowPlaying.sArtist
+        vm.song = data.aNowPlaying.sSong
+    })
+
+console.log(vm.artist)
+    
